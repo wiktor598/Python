37498d7203eb6f85be30d87450b8347bb0c913bf
wiktor598 <113174085+wiktor598@users.noreply.github.com>
2023-04-06 16:51:30 +0100

    Delete PY/Projects directory

--- PY/Projects/Get_Mac.py
+++
@@ -1,3 +0,0 @@
-import uuid 
-
-print(hex(uuid.getnode()))
--- PY/Projects/MostCommon.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-with open('py.txt', 'r') as f:
-    text = f.read()
-
-    c = Counter(text)
-    print(c.most_common())
-
-
