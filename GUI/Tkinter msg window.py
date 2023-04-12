c5c740b3f2385158b77b905e92a18deda9bcec73
Wikus <wpanpuch@loadbalancer.org>
2023-04-12 10:51:35 +0100

    admin update

--- GUI/Tkinter msg window.py
+++ GUI/Tkinter msg window.py
@@ -7,6 +7,31 @@ class MyGUI:
 
         self.root = tk.Tk()
 
+        self.menubar = tk.Menu(self.root)  #Main Menu nav bar
+
+        self.Home = tk.Menu(self.menubar, tearoff=0) #adds 'Home' element to object
+
+        self.Home.add_command(label='Close', command=self.on_closing) # adds close option to 'Home' element
+        self.Home.add_separator()
+        self.Home.add_command(label='Close Without Question', command=exit)
+        self.Home.add_separator()
+        self.Home.add_command(label='Show Message', command=self.show_message)
+
+
+        self.File = tk.Menu(self.menubar, tearoff=0)
+
+        self.File.add_command(label='Save')
+        self.File.add_separator()
+        self.File.add_command(label='Print')
+
+
+        self.menubar.add_cascade(menu=self.Home, label='Home') #add 'Home' to nav bar
+        self.menubar.add_cascade(menu=self.File, label='File') # adds 'File' to navbar
+        self.root.config(menu=self.menubar)
+
+
+
+
         self.label = tk.Label(self.root, text="msg", font=('Arial', 18))
         self.label.pack(padx=10, pady=10)
 
@@ -23,6 +48,10 @@ class MyGUI:
         self.button = tk.Button(self.root, text="Show Message", font=('Arial', 18), command=self.show_message)
         self.button.pack(padx=10, pady=10)
 
+        self.clearbtn = tk.Button(self.root, text="Clear", font=('Arial', 18), command= self.clear)
+        self.clearbtn.pack(padx=10, pady=10)
+
+
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
 
@@ -37,16 +66,19 @@ class MyGUI:
            messagebox.showinfo(title="Message", message=self.textbox.get('1.0', tk.END))
 
     def shortcut(self, event):
-      #  print(event.keysym)
-      # print(event.state)
+    #print(event.keysym)
+    #print(event.state)
 
-      if event.state == 20 and event.keysym == "Return":
+        if event.state == 20 and event.keysym == "Return":
          self.show_message()
 
 
     def on_closing(self):
        if messagebox.askyesno(title="Quit?", message="Do you really want to quit ?"):
         self.root.destroy()
+
+    def clear(self):
+       self.textbox.delete('1.0', tk.END)
 
 
 
