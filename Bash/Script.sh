d71553db98c366789b4952147e89e6894f6561bd
Wikus <wpanpuch@loadbalancer.org>
2023-05-18 16:33:07 +0100

    update

---
+++ Bash/Script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+read -p "File Path " file_path
+read -p "Username " user_name
+read -p "Servername " Server_name 
+read -p "Destination Path " Path
+
+scp ${file_path} ${user_name}@$Server_name:${Path}
+
