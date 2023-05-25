b57d0c6119a5333cac6ea7c5b787f0eb293d5d3b
Wikus <wpanpuch@loadbalancer.org>
2023-05-25 15:11:31 +0100

    updatee

---
+++ Projects/Exceptions.py
@@ -0,0 +1,21 @@
+try:
+
+    a = int(input('Enter a number: '))
+    b = int(input('Enter a number: '))
+    
+    print('{} / {} = '.format(a, b))
+    
+    print(a / b)
+
+except ZeroDivisionError as e:
+    print('Hey, you cannot divide a Number by Zero', e)
+
+except ValueError:
+    print('Invalid Input')
+
+except Exception as e:
+    print('Something went Wrong...')
+
+finally:
+    print('Shutting down')
+    
